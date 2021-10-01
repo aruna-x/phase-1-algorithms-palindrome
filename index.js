967d1b63d274d07cecfcb9c7d57888fc74d1bc0a
Aruna <aruna.evan15@gmail.com>
2021-10-01 09:12:03 -0700

    commit tree 5744b0ef5b8d55fb9b84a09f4d93fc66cc011f0c

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  switch (word.length) {
+    case 0:
+      const newString = prompt("Oops! That was an empty string. Please enter a string here:");
+      return isPalendrome(newString);
+    case 1:
+      return true;
+    default:
+      return palindromeTester(word);
+  }
+}
+
+function palindromeTester(word) {
+  let end = word.length-1;
+
+  for (let startIndex = 0; startIndex < (word.length-1)/2; startIndex++) {
+    const endIndex = end - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+
 }
 
 /* 
   Add your pseudocode here
+  - Accept the string and return edge cases (empty or single char strings)
+  - set up two variables, pointing to the first and last letters
+  - iterate through the string (n/2) times
+  - for odd nums there will be one redundant check (middle char), but not a huge issue?
+  - return false if not matching
+  - else return true
+
 */
 
 /*
   Add written explanation of your solution here
+    - write a function that returns true if the string provided is a palindrome
+    - otherwise return false
+
+  Edge Cases:
+    - when the string is a single character, return true.
+    - prompt user for string if empty
+
 */
 
 // You can run `node index.js` to view these console logs
